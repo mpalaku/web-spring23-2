d8c2014954559ae8a307215cc59ebe1e21b36db4
Madhuri Palakurthi <mpalakurthi10@gmail.com>
2023-05-11 21:25:03 -0700

    HOME WORK AND CLASS

---
+++ class/htmlcss/js/index.js
@@ -0,0 +1,7 @@
+// Javascript we can declare variable - var, let and const are keyword.Keywords are reserved. so we can name it for variable like var var = "maddy";
+
+// var is legacy keyword is not used mostlt trying to see the drawbacks with the code below
+var myName = "Maddy";
+let age = 10;
+let active = true;
+let membership;
